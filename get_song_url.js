32ebd6eab6ec2c9b70fbb960508753005a2deca4
iigmir <roc120j@gmail.com>
2019-04-23 07:32:09 +0800

    func

--- get_song_url.js
+++ get_song_url.js
@@ -1,35 +1,35 @@
 const song_source = require("./song_source");
 const request = require("request");
 const jsdom = require("jsdom");
+const fs = require("fs");
 const { JSDOM } = jsdom;
 
 let all_list = [];
 
-function all_list_closure( callback )
+function get_list(error, response, body)
 {
-    return () => callback();
-}
-
-function get_list(url, song_list)
-{
-    request(url, (error, response, body) =>
-    {
-        if (error) { throw new Error(error); }
-        const { document } = (new JSDOM(body)).window;
-        let new_urls = [
-            ...document.querySelectorAll(".category-page__member a")
-        ].map( u => u.href );
-        console.log(new_urls.length);
-        // all_list_closure( () => all_list.concat(new_urls) );
-        all_list.concat(new_urls);
-        console.log(all_list.length);
-        return new_urls;
-    });
+    if (error) { throw new Error(error); }
+    const { document } = (new JSDOM(body)).window;
+    let new_urls = [
+        ...document.querySelectorAll(".category-page__member a")
+    ].map( u => u.href );
+    all_list.push( ...new_urls );
+    console.log( all_list.length );
+    return;
 }
 
 module.exports = function()
 {
-    let song_list = [];
-    song_source.map( url => song_list = get_list(url, song_list) );
-    return song_list;
+    song_source.map( url =>
+    {
+        request(
+            url,
+            (error, response, body) => get_list(error, response, body)
+        );
+        // fs.writeFile(
+        //     "./output/song_url.js",
+        //     text_render(body),
+        //     (err) => handle_files(err)
+        // );
+    });
 }
